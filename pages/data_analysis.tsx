f92ee92d04fa8bbf93738f2f1e72fa2978c27732
Leonardo Cisternas <leocisternasa@gmail.com>
2022-07-13 09:18:42 -0400

    pasé la foto a jpeg para probar

--- pages/data_analysis.tsx
+++ pages/data_analysis.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Link from 'next/link'
 import Image from 'next/image'
-import projectImage from '../public/IMG_2494.jpg'
+import projectImage from '../public/escalada_pareja.jpeg'
 
 const DataAnalysis: NextPage = () => {
   return (
